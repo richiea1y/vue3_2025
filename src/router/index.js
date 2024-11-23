91f313823fb2313a074ad6fa2e8e0fd9589934fb
AstolfoProto <tokiya.kagami@gmail.com>
2024-11-23 13:57:57 +0800

    fix: change to hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/day-1',
+    name: 'day-1',
+    component: () => import('../views/day-1/day-1.vue')
+  },
+]
+
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/day-1',
-      name: 'day-1',
-      component: () => import('../views/day-1/day-1.vue')
-    },
-  ],
+  history: createWebHashHistory(),
+  routes,
 })
 
 export default router
