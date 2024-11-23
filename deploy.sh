09a17b37afdd6680b054cf76b2ab93e9cb288cd5
richiea1y <44350561+richiea1y@users.noreply.github.com>
2024-11-23 14:22:55 +0800

    add: add repo address to deploy.sh & Add deploy.sh to gitignore

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cd dist
 git init 
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/fortes1219/vue3_2025.git master:gh-pages
+git push -f https://github.com/richiea1y/vue3_2025.git master:gh-pages
 #將dist資料夾中的內容推送至遠端的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -a
